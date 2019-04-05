208b3ce7a7918d90790209004ed08d39927324d1
CTI2019134 <nhakarmacharya@gmail.com>
2019-04-05 14:05:34 +1000

    next attempt.

---
+++ java.js
@@ -0,0 +1,6 @@
+var answer1 = "JavaScript is easy to learn.";
+var answer2 = "This is called 'JavaScript'";
+var answer3 = 'This is called "JavaScript"'; 
+
+document.getElementById("demo").innerHTML =
+answer1 + "<br>" + answer2 + "<br>" + answer3; 
